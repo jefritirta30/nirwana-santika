8ae667945d86be016115f03e40c3b30c3a505c21
AzisDinata29 <azisdinata09@gmail.com>
2025-01-11 00:29:34 +0700

    azis : merubah hero menjadi component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Image from "next/image";
-const Hero = () => {
+
+
+import Hero from "@/components/Hero";
+
+export default function Home() {
   return (
-    <div className="w-full">
-      <Image alt="Jaya Bakery Building" className="w-full" width={10000}
-              height={10000} src='/Logo2.jpeg' />
-    </div>
+    <>
+    <Hero />
+    </>
   );
-};
-export default Hero;
+}
