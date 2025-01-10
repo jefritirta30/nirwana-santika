ebf02f557ca4425e06c2e2fef44940c94d31fbd8
AzisDinata29 <azisdinata09@gmail.com>
2025-01-11 00:30:19 +0700

    azis : mengubah hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import HeroImage from "./../assets/Logo2.jpeg";
 import Image from "next/image";
 const Hero = () => {
   return (
     <div className="w-full">
-      <Image alt="Jaya Bakery Building" className="w-full" src={HeroImage} />
+      <Image alt="Jaya Bakery Building" className="w-full" width={10000}
+              height={10000} src='/Logo2.jpeg' />
     </div>
   );
 };
