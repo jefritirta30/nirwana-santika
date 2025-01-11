a493dd9792d2722b6b1eecd765529197ef4c1ddd
jefritirta30 <jefritirta01@gmail.com>
2025-01-11 23:23:40 +0700

    jefri: membuat hero section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,8 +15,8 @@ const Navbar: React.FC = () => {
             <Image
               src="/logo.png"
               alt="Logo"
-              width={40}
-              height={40}
+              width={45}
+              height={45}
             />
           </div>
           
