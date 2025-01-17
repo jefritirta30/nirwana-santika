ec3ae908158ba45d5b0d99718c00732f5616c21e
AzisDinata29 <azisdinata09@gmail.com>
2025-01-17 08:01:31 +0700

    azis : menambahkan logo di darkmode

--- components/DarkMode.tsx
+++ components/DarkMode.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { FaMoon } from "react-icons/fa";
+import { FaSun } from "react-icons/fa6";
 
 export default function DarkMode() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -24,8 +26,8 @@ export default function DarkMode() {
   };
 
   return (
-    <button onClick={toggleDarkMode} className={`${isDarkMode ? "text-black" : "text-white"}`}>
-      {isDarkMode ? "Light Mode" : "Dark Mode"}
+    <button onClick={toggleDarkMode} className={`${isDarkMode ? "text-white " : "text-black"}`}>
+      {isDarkMode ? <FaSun /> : <FaMoon />} 
     </button>
   );
 }
